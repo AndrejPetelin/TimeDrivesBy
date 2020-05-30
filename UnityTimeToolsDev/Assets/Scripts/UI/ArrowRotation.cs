1cebeef9dc62d4cdd9737677792bb52fdff0a64d
irenenaya <irenayamackay@gmail.com>
2020-05-30 19:29:15 -0300

    direction arrow

---
+++ UnityTimeToolsDev/Assets/Scripts/UI/ArrowRotation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ArrowRotation : MonoBehaviour
+{
+    public Transform player;
+    public Transform target;
+    RectTransform rect;
+    Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rect =GetComponent<RectTransform>();
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       /* Vector3 dir = transform.InverseTransformDirection(target.transform.position - player.transform.position);
+        float angle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg;
+        rect.eulerAngles = new Vector3(0, 0, angle);*/
+
+        var targetPosLocal = cam.transform.InverseTransformPoint(target.position);
+        var targetAngle = -Mathf.Atan2(targetPosLocal.x, targetPosLocal.y) * Mathf.Rad2Deg ;
+        rect.eulerAngles = new Vector3(0, 0, targetAngle);
+    }
+}
