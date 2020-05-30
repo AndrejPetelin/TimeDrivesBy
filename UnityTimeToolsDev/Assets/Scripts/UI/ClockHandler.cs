a255285760d889c8c0fc0b71518e6704dea92a1b
Andrej Petelin <petelin.andrej@amis.net>
2020-05-30 06:03:47 +0200

    Broken clock that isn't even right twice a day

---
+++ UnityTimeToolsDev/Assets/Scripts/UI/ClockHandler.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClockHandler : MonoBehaviour
+{
+
+    public GameObject targetHandPrefab;
+
+    GameObject targetHand;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetHand = Instantiate(targetHandPrefab);
+        targetHand.transform.position = transform.position;
+        targetHand.transform.rotation = transform.rotation;
+        
+        targetHand.transform.SetParent(transform.parent);
+        targetHand.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ShowTargetHand()
+    {
+        targetHand.SetActive(true);
+    }
+
+
+    void HideTargetHand()
+    {
+        targetHand.SetActive(false);
+    }
+
+
+    void DragHand()
+    {
+
+    }
+    
+}
