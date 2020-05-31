7a1926b2beaedd37619fc4498df782b9d80f8385
tiannty <46940133+tiannty@users.noreply.github.com>
2020-06-01 01:14:28 +0300

    Added level

---
+++ UnityTimeToolsDev/Assets/Scripts/GameManagement/LevelManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour
+{
+	//public GameObject loadingPanel;
+	//public Slider loadingProgressBar;
+		
+     public void LoadLevel(int sceneIndex)
+    {
+		if (sceneIndex < SceneManager.sceneCountInBuildSettings)
+		{
+			StartCoroutine(LoadAsync(sceneIndex));
+		}
+		else  //TODO
+		{
+			StartCoroutine(LoadAsync(0));
+		}
+		
+	}
+
+    IEnumerator LoadAsync(int sceneIndex)
+	{
+		AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneIndex);
+
+		//loadingPanel.SetActive(true);
+		while (!asyncLoad.isDone)
+		{
+			//float progress = Mathf.Clamp01(asyncLoad.progress / 0.9f);
+			//loadingProgressBar.value = progress;
+			//Debug.Log(progress);
+			yield return null;
+		}
+	}
+}
