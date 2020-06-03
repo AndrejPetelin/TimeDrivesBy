d6f8d89a434c5b23238937e3e3aeef8fbd637494
tiannty <46940133+tiannty@users.noreply.github.com>
2020-06-04 02:18:43 +0300

    Added PausedScene additive loader

---
+++ UnityTimeToolsDev/Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PauseMenu : MonoBehaviour
+{
+	private string menuScene = "MainMenuScene2";
+
+
+	public void BackToMainMenu()
+	{
+		FindObjectOfType<PausedSceneLoader>().ResumeGame();
+		SceneManager.LoadScene(menuScene);
+	}
+
+	public void OnResumeGame()
+	{
+		FindObjectOfType<PausedSceneLoader>().ResumeGame();
+	}
+}
---
+++ UnityTimeToolsDev/Assets/Scripts/UI/PausedSceneLoader.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PausedSceneLoader : MonoBehaviour
+{
+	private string pauseScene = "PausedScene3";
+	public bool isPaused = false;
+	bool isLoaded = false;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			if (!isPaused)
+			{
+				PauseGame();				
+			}
+			else
+			{
+				ResumeGame();
+			}
+		}
+	}
+
+
+	void PauseGame()
+	{
+		isPaused = true;
+		Time.timeScale = 0f;
+		SceneManager.LoadScene(pauseScene, LoadSceneMode.Additive);
+		isLoaded = true;
+	}
+
+
+	public void ResumeGame()
+	{
+		isPaused = false;
+		Time.timeScale = 1f;
+		if (isLoaded)
+		{
+			SceneManager.UnloadSceneAsync(pauseScene);
+			isLoaded = false;
+		}
+	}
+}
